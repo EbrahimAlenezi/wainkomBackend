69afbadf0f3669f1d4db1c1fb801737aa0e769e7
Sayuri Cero <Hisaalqaoud@gmail.com>
2025-10-02 20:00:04 +0300

    small fix

--- src/api/User/User.router.ts
+++ src/api/User/User.router.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getUsers, updateUser, deleteUser, deleteAllUsers,getuserById } from "./User.controller";
+import { getUsers, updateUser, deleteUser, deleteAllUsers, getUserById } from "./User.controller";
 import { authorize } from "../../Middleware/authorization";
 import upload from "../../Middleware/multer";
 
 const userRoutes = Router();
 
 userRoutes.get("/", getUsers);
-userRoutes.get("/:id", getuserById);
+userRoutes.get("/:id", getUserById);
 
 userRoutes.put("/", authorize, upload.single("image"), updateUser);
 userRoutes.delete("/", authorize, deleteUser);
