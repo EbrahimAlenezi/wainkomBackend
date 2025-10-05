aae81b6d620127620d1d6f52b653dfe921b4f044
Abdullah <abdullah@Abdullahs-MacBook-Air.local>
2025-10-05 20:33:03 +0300

    changes

--- src/Middleware/authorization.ts
+++ src/Middleware/authorization.ts
@@ -4,11 +4,15 @@ import jwt from 'jsonwebtoken';
 
 export const authorize = async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(" ")[1];
+    console.log(token);
     if (!token) return res.status(401).json({ message: "No token provided" });
     try {
       const decoded = jwt.verify(token, process.env.JWT_Secret!) as { _id: string };
+      console.log(decoded);
       const user = await User.findById(decoded._id);
+      console.log(user);
       if (!user) return res.status(401).json({ message: "Invalid token" });
+      console.log("user found");
       req.user = user; // now req.user is typed as UserDoc
       next();
     } catch (error) {
