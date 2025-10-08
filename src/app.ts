84fb1a9cbd3f145c12b6c55cadcdac44dc95c6ae
EbrahimAlenezi <ibrahemalwteed@gmail.com>
2025-10-08 02:01:32 +0100

    last updaaaaaate

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(cors());
 
 // serve uploaded files
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-
+app.use("/uploads", express.static(path.resolve("uploads")));
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/user", userRoutes);
